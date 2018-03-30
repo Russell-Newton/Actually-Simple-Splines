7afb6f55fde2947382e1b125586cbeeca58bec3f
NamelessSuperCoder <Marius.juston@hotmail.fr>
2018-03-29 21:22:55 -0400

    fixed turn angle by giving it a start pose.

--- src/org/waltonrobotics/motion/PointTurn.java
+++ src/org/waltonrobotics/motion/PointTurn.java
@@ -8,31 +8,32 @@ import org.waltonrobotics.controller.State;
 
 public class PointTurn extends Path {
 
-	private final double startAngle;
+	private final Pose startPose;
 	private final double endAngle;
 	private final double turnStep;
 	private final double vRotationMax;
 	private final double aRotationMax;
 	private final LinkedList<PathData> pathData;
 
-	public PointTurn(double vRotationMax, double aRotationMax, double startAngle, double endAngle) {
+	public PointTurn(double vRotationMax, double aRotationMax, Pose startPosition,
+		double endAngle) {
 		super(vRotationMax, aRotationMax, false,
-			new Pose(0, 0, startAngle), new Pose(0, 0, endAngle));
-		this.startAngle = startAngle;
+			startPosition, new Pose(startPosition.getX(), startPosition.getY(), endAngle));
+		startPose = startPosition;
 		this.endAngle = endAngle;
 
 		this.vRotationMax = vRotationMax;
 		this.aRotationMax = aRotationMax;
-		double turnAngle = this.endAngle - this.startAngle;
+		double turnAngle = this.endAngle - startPose.getAngle();
 		turnAngle = boundAngle(turnAngle);
 
 		turnStep = turnAngle / getPathNumberOfSteps();
-		this.pathData = new LinkedList<>();
+		pathData = new LinkedList<>();
 		generateData();
 	}
 
 	private void generateData() {
-		pathData.add(getPathData(new PathData(new Pose(0, 0, startAngle))));
+		pathData.add(getPathData(new PathData(startPose)));
 		for (int i = 1; i < getPathNumberOfSteps(); i++) {
 			pathData.add(getPathData(pathData.getLast()));
 		}
@@ -41,8 +42,8 @@ public class PointTurn extends Path {
 	private PathData getPathData(PathData previous) {
 
 		// estimate lengths each wheel will turn
-		double dlLeft = (-turnStep * getRobotWidth() / 2);
-		double dlRight = (turnStep * getRobotWidth() / 2);
+		double dlLeft = ((-turnStep * getRobotWidth()) / 2.0);
+		double dlRight = ((turnStep * getRobotWidth()) / 2.0);
 
 		// assuming one of the wheels will limit motion, calculate time this step will
 		// take
@@ -51,27 +52,28 @@ public class PointTurn extends Path {
 
 		// assuming constant angular acceleration from/to zero angular speed
 		double omega = Math.abs(dlRight - dlLeft) / dt / getRobotWidth();
-		double thetaMidpoint = previous.getCenterPose().getAngle() + .5 * turnStep;
+		double thetaMidpoint = previous.getCenterPose().getAngle() + (0.5 * turnStep);
 
 		double omegaAccel = Math.sqrt(aRotationMax
-			* Math.abs(boundAngle(thetaMidpoint - startAngle)));
+			* Math.abs(boundAngle(thetaMidpoint - startPose.getAngle())));
 		double omegaDecel = Math.sqrt(aRotationMax
 			* Math.abs(boundAngle(thetaMidpoint - endAngle)));
 //		System.out.println("OmegaAccel=" + omegaAccel);
 //         System.out.println(omega);
-		if (omegaAccel < omega && omegaAccel < omegaDecel) {
+		if ((omegaAccel < omega) && (omegaAccel < omegaDecel)) {
 			dt = Math.abs(dlRight - dlLeft) / omegaAccel / getRobotWidth();
 		}
 
 //		System.out.println("OmegaDecel=" + omegaDecel);
-		if (omegaDecel < omega && omegaDecel < omegaAccel) {
+		if ((omegaDecel < omega) && (omegaDecel < omegaAccel)) {
 			dt = Math.abs(dlRight - dlLeft) / omegaDecel / getRobotWidth();
 		}
 //        System.out.println(dt);
 
 		State left = new State(previous.getLeftState().getLength() + dlLeft, dlLeft / dt, a);
 		State right = new State(previous.getRightState().getLength() + dlRight, dlRight / dt, a);
-		Pose center = new Pose(0, 0, previous.getCenterPose().getAngle() + turnStep);
+		Pose center = new Pose(startPose.getX(), startPose.getY(),
+			previous.getCenterPose().getAngle() + turnStep);
 
 		return new PathData(left, right, center, previous.getTime() + dt);
 	}
@@ -79,5 +81,17 @@ public class PointTurn extends Path {
 	@Override
 	public LinkedList<PathData> getPathData() {
 		return pathData;
+	}
+
+	@Override
+	public String toString() {
+		return "PointTurn{" +
+			"startPose=" + startPose +
+			", endAngle=" + endAngle +
+			", turnStep=" + turnStep +
+			", vRotationMax=" + vRotationMax +
+			", aRotationMax=" + aRotationMax +
+			", pathData=" + pathData +
+			"} " + super.toString();
 	}
 }
