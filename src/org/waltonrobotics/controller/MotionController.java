a789eeb8de4602d0f8977a2c4f7981bcc2c9170d
ThundrHawk <rnewtonkids001@gmail.com>
2018-03-31 09:39:01 -0400

    Add the getPercentDone method
    Will return the percent of the current path that has been completed
    (based off of time)

--- src/org/waltonrobotics/controller/MotionController.java
+++ src/org/waltonrobotics/controller/MotionController.java
@@ -325,6 +325,15 @@ public class MotionController {
 	public final boolean isRunning() {
 		return running;
 	}
+	
+	/**
+	 * @return Percent of the current Path that the robot is at, based off of the time
+	 */
+	public double getPercentDone() {
+		double currentTime = drivetrain.getWheelPositions().getTime() - pathStartTime;
+		double endTime = currentPath.getPathData().getLast().getTime();
+		return currentTime/endTime;
+	}
 
 	/**
 	 * Pauses the motions,
