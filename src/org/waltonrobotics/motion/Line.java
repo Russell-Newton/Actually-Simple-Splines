d6ec045988821fe845b338ada5f0df8dda9a8ffb
NamelessSuperCoder <Marius.juston@hotmail.fr>
2018-04-10 18:00:38 -0400

    added constructor that takes in just distance as parameter

--- src/org/waltonrobotics/motion/Line.java
+++ src/org/waltonrobotics/motion/Line.java
@@ -6,17 +6,16 @@ import org.waltonrobotics.controller.Pose;
  * <p>
  * This Path creates a straight line using the BezierCurve class.
  * </p>
- * 
+ *
  * @author Russell Newton, Walton Robotics
  * @see BezierCurve
- *
  */
 public class Line extends BezierCurve {
 
 	/**
-	 * Be careful when using this. If your robot's angle is off, the MotionContoller
-	 * will try to correct for it, so you will not get a straight line.
-	 * 
+	 * Be careful when using this. If your robot's angle is off, the MotionContoller will try to correct for it, so you
+	 * will not get a straight line.
+	 *
 	 * @param vCruise - the cruise velocity of the robot
 	 * @param aMax - the maximum acceleration of the robot
 	 * @param startVelocity - the start velocity
@@ -25,12 +24,25 @@ public class Line extends BezierCurve {
 	 * @param startPose - the starting Pose. Angle doesn't matter
 	 * @param endPose - the ending Pose. Angle doesn't matter
 	 */
-	public Line(double vCruise, double aMax, double v0, double v1, boolean isBackwards, Pose startPose, Pose endPose) {
-		super(vCruise, aMax, v0, v1, isBackwards, startPose, endPose);
+	public Line(double vCruise, double aMax, double startVelocity, double endVelocity, boolean isBackwards,
+		Pose startPose, Pose endPose) {
+		super(vCruise, aMax, startVelocity, endVelocity, isBackwards, startPose, endPose);
 	}
-	
-	/*
-	 * Evan this is probably the silliest class I've ever written, but here you go.
-	 */
 
+	/**
+	 * Be careful when using this. If your robot's angle is off, the MotionContoller will try to correct for it, so you
+	 * will not get a straight line.
+	 *
+	 * @param vCruise - the cruise velocity of the robot
+	 * @param aMax - the maximum acceleration of the robot
+	 * @param startVelocity - the start velocity
+	 * @param endVelocity - the end velocity
+	 * @param isBackwards - whether or not to move the robot backwards
+	 * @param startPose - the starting Pose. Angle doesn't matter
+	 * @param distance - how you wish to offset the startingPose from.
+	 */
+	public Line(double vCruise, double aMax, double startVelocity, double endVelocity, boolean isBackwards,
+		Pose startPose, double distance) {
+		this(vCruise, aMax, startVelocity, endVelocity, isBackwards, startPose, startPose.offset(distance));
+	}
 }
