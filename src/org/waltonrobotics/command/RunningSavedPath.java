c83fc4dac875c2eef47924af6349a6e67e6086a9
NamelessSuperCoder <Marius.juston@hotmail.fr>
2018-05-28 22:52:08 -0400

    added the abilty to make a saved motion faster

--- src/org/waltonrobotics/command/RunningSavedPath.java
+++ src/org/waltonrobotics/command/RunningSavedPath.java
@@ -9,6 +9,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.waltonrobotics.controller.Moment;
+import org.waltonrobotics.controller.RobotPair;
 import org.waltonrobotics.motion.SavePath;
 
 public final class RunningSavedPath extends SimpleMotion {
@@ -19,11 +20,15 @@ public final class RunningSavedPath extends SimpleMotion {
 	}
 
 	private RunningSavedPath(Moment... movement) {
-		super(SavePath.createPathData(movement));
+		this(Arrays.asList(movement));
 	}
 
 
 	public static RunningSavedPath loadSavedPath(String filePath) {
+		return loadSavedPath(filePath, 1);
+	}
+
+	public static RunningSavedPath loadSavedPath(String filePath, double speedIncrease) {
 		try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(filePath)))) {
 
 			List<Moment> moments = bufferedReader.lines().map(
@@ -33,7 +38,7 @@ public final class RunningSavedPath extends SimpleMotion {
 				}
 			).collect(Collectors.toList());
 
-			return new RunningSavedPath(moments);
+			return new RunningSavedPath(scaleTime(moments, speedIncrease));
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -43,8 +48,28 @@ public final class RunningSavedPath extends SimpleMotion {
 		return new RunningSavedPath();
 	}
 
+	/**
+	 * Will scale time to be short the bigger the number. E.i. passing in 3 means that time will go 3 time faster
+	 * meaning that the robot will complete the motion faster.
+	 */
+	private static List<Moment> scaleTime(List<Moment> moments, double inverseScale) {
+		return moments.stream().map(moment ->
+			new Moment(
+				moment.getActualPosition(),
+				new RobotPair(
+					moment.getLeftLength(),
+					moment.getRightLength(),
+					moment.getTime() / inverseScale)
+			)
+		).collect(Collectors.toList());
+	}
+
 	public static RunningSavedPath loadSavedPath(LearnPath learnedPath) {
-		return new RunningSavedPath(learnedPath.getMoments());
+		return loadSavedPath(learnedPath, 1);
+	}
+
+	public static RunningSavedPath loadSavedPath(LearnPath learnedPath, double speedIncrease) {
+		return new RunningSavedPath(scaleTime(learnedPath.getMoments(), speedIncrease));
 	}
 
 }
