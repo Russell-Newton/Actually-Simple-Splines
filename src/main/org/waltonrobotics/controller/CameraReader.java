eef51664ab8e7cabf2df7f96430233a52aa8fa10
NamelessSuperCoder <Marius.juston@hotmail.fr>
2019-02-07 22:51:19 -0500

    simple reformatting

--- src/main/org/waltonrobotics/controller/CameraReader.java
+++ src/main/org/waltonrobotics/controller/CameraReader.java
@@ -22,23 +22,25 @@ public class CameraReader extends TimerTask {
 
   @Override
   public synchronized void run() {
-      String data = serialPort.readString();
-      if(data.charAt(0) != 'F') {
-        double x = Double.parseDouble(data.substring(1, 4)) *
-            data.charAt(0) == 'X' ? 1 : -1;
-        double y = Double.parseDouble(data.substring(5, 8)) *
-            data.charAt(4) == 'Y' ? 1 : -1;
-        double z = Double.parseDouble(data.substring(9, 11)) *
-            data.charAt(8) == 'Z' ? 1 : -1;
-        double angle = Double.parseDouble(data.substring(12, 15)) *
-            data.charAt(11) == 'A' ? 1 : -1;
-        int numberOfTargets = Integer.parseInt(data.substring(16));
-        double t = Timer.getFPGATimestamp() - 0.052;
-        this.cameraData = new CameraData(x, y, z, angle, numberOfTargets, t);
-      } else {
-        int numberOfTargets = Integer.parseInt(data.substring(2));
-        this.cameraData = new CameraData(numberOfTargets);
-      }
+    String data = serialPort.readString();
+    if (data.charAt(0) != 'F') {
+      double x = Double.parseDouble(data.substring(1, 4)) *
+          data.charAt(0) == 'X' ? 1 : -1;
+      double y = Double.parseDouble(data.substring(5, 8)) *
+          data.charAt(4) == 'Y' ? 1 : -1;
+      double z = Double.parseDouble(data.substring(9, 11)) *
+          data.charAt(8) == 'Z' ? 1 : -1;
+      double angle = Double.parseDouble(data.substring(12, 15)) *
+          data.charAt(11) == 'A' ? 1 : -1;
+      int numberOfTargets = Integer.parseInt(data.substring(16));
+      double t = Timer.getFPGATimestamp() - 0.052;
+      this.cameraData = new CameraData(x, y, z, angle, numberOfTargets, t);
+    } else {
+      int numberOfTargets = Integer.parseInt(data.substring(2));
+      this.cameraData = new CameraData(numberOfTargets);
+    }
+  }
+
   public void startCollecting() {
     serialPort.writeString("S");
   }
