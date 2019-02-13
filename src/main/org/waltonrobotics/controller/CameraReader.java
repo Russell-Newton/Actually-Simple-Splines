a16c94777ac4d9474f48b408030118b6ed0c313e
NamelessSuperCoder <Marius.juston@hotmail.fr>
2019-02-12 23:17:04 -0500

    changed buffer size from 25 to 1
    allows instanteuous response from serial port

--- src/main/org/waltonrobotics/controller/CameraReader.java
+++ src/main/org/waltonrobotics/controller/CameraReader.java
@@ -13,7 +13,7 @@ public class CameraReader extends TimerTask {
   public CameraReader() {
     serialPort = new SerialPort(115200, Port.kUSB);
     serialPort.enableTermination();
-    serialPort.setReadBufferSize(25);
+    serialPort.setReadBufferSize(1);
   }
 
   public CameraData getCameraData() {
