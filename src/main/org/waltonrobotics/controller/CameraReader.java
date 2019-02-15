f607f88fca16a8ce5bb45831e379e51e8d4a72d9
NamelessSuperCoder <Marius.juston@hotmail.fr>
2019-02-14 23:28:14 -0500

    improved camera data validation
    uses an regex expression in order to make sure that the string is formatted correctly instead of checks such as lengths

--- src/main/org/waltonrobotics/controller/CameraReader.java
+++ src/main/org/waltonrobotics/controller/CameraReader.java
@@ -23,28 +23,32 @@ public class CameraReader extends TimerTask {
   @Override
   public synchronized void run() {
     String data = serialPort.readString().trim();
-    if (data.length() > 17) {
-      if (data.charAt(0) != 'F') {
-        double x = Double.parseDouble(data.substring(1, 4)) *
-            (data.charAt(0) == 'X' ? 1 : -1);
-        double y = Double.parseDouble(data.substring(5, 8)) *
-            (data.charAt(4) == 'Y' ? 1 : -1);
-        double z = Double.parseDouble(data.substring(9, 12)) *
-            (data.charAt(8) == 'Z' ? 1 : -1);
-        double angle = Double.parseDouble(data.substring(13, 16)) *
-            (data.charAt(12) == 'A' ? 1 : -1);
-        int numberOfTargets = Integer.parseInt(data.substring(17));
-        double t = Timer.getFPGATimestamp() - 0.052;
 
-        x /= 100;
-        y /= 100;
+//    System.out.println(data.matches("(F)|(\\d{2,})"));
+    System.out.println(data);
 
-        this.cameraData = new CameraData(x, y, z, angle, numberOfTargets, t);
-      } else {
-        int numberOfTargets = Integer.parseInt(data.substring(2));
-        this.cameraData = new CameraData(numberOfTargets);
-      }
-    }else{
+
+//    if (data.length() > 17) {
+    if (data.matches("^[xX][ \\d]{3}[yY][ \\d]{3}[zZ][ \\d]{3}[aA][ \\d]{3}N\\d+$")) {
+      double x = Double.parseDouble(data.substring(1, 4)) *
+          (data.charAt(0) == 'X' ? 1 : -1);
+      double y = Double.parseDouble(data.substring(5, 8)) *
+          (data.charAt(4) == 'Y' ? 1 : -1);
+      double z = Double.parseDouble(data.substring(9, 12)) *
+          (data.charAt(8) == 'Z' ? 1 : -1);
+      double angle = Double.parseDouble(data.substring(13, 16)) *
+          (data.charAt(12) == 'A' ? 1 : -1);
+      int numberOfTargets = Integer.parseInt(data.substring(17));
+      double t = Timer.getFPGATimestamp() - 0.052;
+
+      x /= 100;
+      y /= 100;
+
+      this.cameraData = new CameraData(x, y, z, angle, numberOfTargets, t);
+    } else if (data.matches("^FN\\d+$")) {
+      int numberOfTargets = Integer.parseInt(data.substring(2));
+      this.cameraData = new CameraData(numberOfTargets);
+    } else {
       this.cameraData = new CameraData();
     }
   }
