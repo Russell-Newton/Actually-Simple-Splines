47240752f29360f264763c60fd86e6363c849e86
NamelessSuperCoder <Marius.juston@hotmail.fr>
2019-02-15 21:29:41 -0500

    gradle implementation of the project

--- Example/build/gradlerio_shuffleboard.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-setlocal
-pushd C:\Users\mariu\.gradle\gradlerio\Shuffleboard
-java "-jar" "C:\Users\mariu\.gradle\caches\modules-2\files-2.1\edu.wpi.first.shuffleboard\app\1.1.1\a4df8f16b985a4fcb5cf2247f27887fb9d12fd2a\app-1.1.1.jar"
-popd
-endlocal
