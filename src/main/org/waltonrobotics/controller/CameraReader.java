bf925593c163c069a2226c8c0bba7bcaf70abb4e
NamelessSuperCoder <Marius.juston@hotmail.fr>
2019-02-15 19:00:00 -0500

    improved efficiency of camera reader
    improved regex to validate the serial output instead of using Double.parseDouble use Integer.parseUnsignedInt which is more efficient

--- src/main/org/waltonrobotics/controller/CameraReader.java
+++ src/main/org/waltonrobotics/controller/CameraReader.java
@@ -29,24 +29,21 @@ public class CameraReader extends TimerTask {
 
 
 //    if (data.length() > 17) {
-    if (data.matches("^[xX][ \\d]{3}[yY][ \\d]{3}[zZ][ \\d]{3}[aA][ \\d]{3}N\\d+$")) {
-      double x = Double.parseDouble(data.substring(1, 4)) *
+    if (data.matches("^[xX]\\d{3}[yY]\\d{3}[zZ]\\d{3}[aA]\\d{3}N\\d+$")) {
+      int x = Integer.parseUnsignedInt(data.substring(1, 4)) *
           (data.charAt(0) == 'X' ? 1 : -1);
-      double y = Double.parseDouble(data.substring(5, 8)) *
+      int y = Integer.parseUnsignedInt(data.substring(5, 8)) *
           (data.charAt(4) == 'Y' ? 1 : -1);
-      double z = Double.parseDouble(data.substring(9, 12)) *
+      int z = Integer.parseUnsignedInt(data.substring(9, 12)) *
           (data.charAt(8) == 'Z' ? 1 : -1);
-      double angle = Double.parseDouble(data.substring(13, 16)) *
+      int angle = Integer.parseUnsignedInt(data.substring(13, 16)) *
           (data.charAt(12) == 'A' ? 1 : -1);
-      int numberOfTargets = Integer.parseInt(data.substring(17));
+      int numberOfTargets = Integer.parseUnsignedInt(data.substring(17));
       double t = Timer.getFPGATimestamp() - 0.052;
 
-      x /= 100;
-      y /= 100;
-
-      this.cameraData = new CameraData(x, y, z, angle, numberOfTargets, t);
+      this.cameraData = new CameraData(x / 100.0, y / 100.0, z, angle, numberOfTargets, t);
     } else if (data.matches("^FN\\d+$")) {
-      int numberOfTargets = Integer.parseInt(data.substring(2));
+      int numberOfTargets = Integer.parseUnsignedInt(data.substring(2));
       this.cameraData = new CameraData(numberOfTargets);
     } else {
       this.cameraData = new CameraData();
